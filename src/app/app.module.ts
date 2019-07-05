d87e88de447cf754e3929b19a13f6581cf28a051
Luzexiao <2312588031@qq.com>
2019-07-05 12:53:21 +0800

    addfile

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
+
 import { NgZorroAntdMobileModule } from 'ng-zorro-antd-mobile';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule,NgZorroAntdMobileModule ],
+  imports:      [ BrowserModule, FormsModule, NgZorroAntdMobileModule ],
   declarations: [ AppComponent, HelloComponent, HomeComponent ],
   bootstrap:    [ AppComponent ]
 })
