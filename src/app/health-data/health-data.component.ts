952615723e09064e307ba83ae68bd3165a80e009
Luzexiao <2312588031@qq.com>
2019-07-09 15:26:54 +0800

    edit file

--- src/app/health-data/health-data.component.ts
+++ src/app/health-data/health-data.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 @Component({
   selector: 'app-health-data',
@@ -11,136 +11,135 @@ export class HealthDataComponent implements OnInit {
   ) { }
   chartOption: any;
   ngOnInit() {
-    this.chartOption ={
+    this.chartOption = {
       tooltip: {
-          trigger: 'axis'
+        trigger: 'axis'
       },
       legend: {
-          color: ["#F58080", "#47D8BE"],
-          data: ['收缩压', '舒张压'],
-          left: 'center',
-          bottom: '3%'
+        color: ["#F58080", "#47D8BE"],
+        data: ['收缩压', '舒张压'],
+        top: '15',
+        right: '3%'
       },
       grid: {
-          top: 'middle',
-          left: '3%',
-          right: '4%',
-          bottom: '3%',
-          height: '80%',
-          containLabel: true
+        left: '3%',
+        right: '3%',
+        bottom: '10',
+        height: '72%',
+        containLabel: true
       },
       xAxis: {
-          type: 'category',
-          data: [100,200,20,30,60,89],
-          axisLine: {
-              lineStyle: {
-                  color: "#999"
-              }
+        type: 'category',
+        data: [100, 200, 20, 30, 60, 89],
+        axisLine: {
+          lineStyle: {
+            color: "#999"
           }
+        }
       },
       yAxis: {
-          type: 'value',
-          name:'血压平均值',
-          splitLine: {
-              lineStyle: {
-                  type: 'dashed',
-                  color: '#DDD'
-              }
-          },
-          axisLine: {
-              show: false,
-              lineStyle: {
-                  color: "#333"
-              },
-          },
-          nameTextStyle: {
-              color: "#999"
-          },
-          splitArea: {
-              show: false
+        type: 'value',
+        name: '血压平均值',
+        splitLine: {
+          lineStyle: {
+            type: 'dashed',
+            color: '#DDD'
           }
+        },
+        axisLine: {
+          show: false,
+          lineStyle: {
+            color: "#333"
+          },
+        },
+        nameTextStyle: {
+          color: "#999"
+        },
+        splitArea: {
+          show: false
+        }
       },
       series: [{
-              name: '收缩压',
-              type: 'line',
-              data: [800,900,220,130,660,289],
-              color: "#F58080",
-              lineStyle: {
-                  normal: {
-                      width: 5,
-                      color: {
-                          type: 'linear',
-  
-                          colorStops: [{
-                              offset: 0,
-                              color: '#FFCAD4' // 0% 处的颜色
-                          }, {
-                              offset: 0.4,
-                              color: '#F58080' // 100% 处的颜色
-                          }, {
-                              offset: 1,
-                              color: '#F58080' // 100% 处的颜色
-                          }],
-                          globalCoord: false // 缺省为 false
-                      },
-                      shadowColor: 'rgba(245,128,128, 0.5)',
-                      shadowBlur: 10,
-                      shadowOffsetY: 7
-                  }
-              },
-              itemStyle: {
-                  normal: {
-                      color: '#F58080',
-                      borderWidth: 10,
-                      /*shadowColor: 'rgba(72,216,191, 0.3)',
-                       shadowBlur: 100,*/
-                      borderColor: "#F58080"
-                  }
-              },
-              smooth: true
-          },
-          {
-              name: '舒张压',
-              type: 'line',
-              data: [123,568,111,222,123,56],
-              lineStyle: {
-                  normal: {
-                      width: 5,
-                      color: {
-                          type: 'linear',
-  
-                          colorStops: [{
-                                  offset: 0,
-                                  color: '#AAF487' // 0% 处的颜色
-                              },
-                              {
-                                  offset: 0.4,
-                                  color: '#47D8BE' // 100% 处的颜色
-                              }, {
-                                  offset: 1,
-                                  color: '#47D8BE' // 100% 处的颜色
-                              }
-                          ],
-                          globalCoord: false // 缺省为 false
-                      },
-                      shadowColor: 'rgba(71,216,190, 0.5)',
-                      shadowBlur: 10,
-                      shadowOffsetY: 7
-                  }
-              },
-              itemStyle: {
-                  normal: {
-                      color: '#AAF487',
-                      borderWidth: 10,
-                      /*shadowColor: 'rgba(72,216,191, 0.3)',
-                       shadowBlur: 100,*/
-                      borderColor: "#AAF487"
-                  }
-              },
-              smooth: true
+        name: '收缩压',
+        type: 'line',
+        data: [800, 900, 220, 130, 660, 289],
+        color: "#F58080",
+        lineStyle: {
+          normal: {
+            width: 5,
+            color: {
+              type: 'linear',
+
+              colorStops: [{
+                offset: 0,
+                color: '#FFCAD4' // 0% 处的颜色
+              }, {
+                offset: 0.4,
+                color: '#F58080' // 100% 处的颜色
+              }, {
+                offset: 1,
+                color: '#F58080' // 100% 处的颜色
+              }],
+              globalCoord: false // 缺省为 false
+            },
+            shadowColor: 'rgba(245,128,128, 0.5)',
+            shadowBlur: 10,
+            shadowOffsetY: 7
           }
+        },
+        itemStyle: {
+          normal: {
+            color: '#F58080',
+            borderWidth: 10,
+            /*shadowColor: 'rgba(72,216,191, 0.3)',
+             shadowBlur: 100,*/
+            borderColor: "#F58080"
+          }
+        },
+        smooth: true
+      },
+      {
+        name: '舒张压',
+        type: 'line',
+        data: [123, 568, 111, 222, 123, 56],
+        lineStyle: {
+          normal: {
+            width: 5,
+            color: {
+              type: 'linear',
+
+              colorStops: [{
+                offset: 0,
+                color: '#AAF487' // 0% 处的颜色
+              },
+              {
+                offset: 0.4,
+                color: '#47D8BE' // 100% 处的颜色
+              }, {
+                offset: 1,
+                color: '#47D8BE' // 100% 处的颜色
+              }
+              ],
+              globalCoord: false // 缺省为 false
+            },
+            shadowColor: 'rgba(71,216,190, 0.5)',
+            shadowBlur: 10,
+            shadowOffsetY: 7
+          }
+        },
+        itemStyle: {
+          normal: {
+            color: '#AAF487',
+            borderWidth: 10,
+            /*shadowColor: 'rgba(72,216,191, 0.3)',
+             shadowBlur: 100,*/
+            borderColor: "#AAF487"
+          }
+        },
+        smooth: true
+      }
       ]
+    }
   }
-}
   index = 0;
 }
